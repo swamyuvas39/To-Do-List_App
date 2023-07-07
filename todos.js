e6b205ff4fa07ac7e793587f23b1f871a6a41c87
swamyuvas39 <swamyuvas@gmail.com>
2023-07-07 13:00:33 +0530

    initial commit

---
+++ todos.js
@@ -0,0 +1,90 @@
+const todoNameInput = document.getElementById('todo-name');
+const todoDescInput = document.getElementById('todo-description');
+const addItemBtn = document.getElementById('add-item');
+const remainingList = document.getElementById('remaining-list');
+const doneList = document.getElementById('done-list');
+
+addItemBtn.addEventListener('click', addTodo);
+
+// CRUD CRUD API endpoint
+const apiBaseUrl = 'https://crudcrud.com/api/9017b161dcd748808eca596fc6a3a311';
+
+function addTodo() {
+    const todoName = todoNameInput.value.trim();
+    const todoDesc = todoDescInput.value.trim();
+
+    if (todoName !== '') {
+        const todoItem = createTodoItem(todoName, todoDesc);
+        remainingList.appendChild(todoItem);
+        todoNameInput.value = '';
+        todoDescInput.value = '';
+
+        // Save the new to-do item to the API
+        saveTodoToAPI({ name: todoName, description: todoDesc });
+    }
+}
+
+function createTodoItem(name, description) {
+    const todoItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const label = document.createElement('label');
+    label.textContent = name;
+    const desc = document.createElement('p');
+    desc.textContent = description;
+
+    todoItem.classList.add('todo-item');
+    todoItem.appendChild(checkbox);
+    todoItem.appendChild(label);
+    todoItem.appendChild(desc);
+
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            todoItem.classList.add('completed');
+            remainingList.removeChild(todoItem);
+            doneList.appendChild(todoItem);
+        } else {
+            todoItem.classList.remove('completed');
+            doneList.removeChild(todoItem);
+            remainingList.appendChild(todoItem);
+        }
+    });
+
+    return todoItem;
+}
+
+function saveTodoToAPI(todo) {
+    const endpoint = `${apiBaseUrl}/todos`;
+    fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    .then(response => response.json())
+    .then(data => console.log('Saved to-do:', data))
+    .catch(error => console.error('Error saving to-do:', error));
+}
+
+function loadTodosFromAPI() {
+    const endpoint = `${apiBaseUrl}/todos`;
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(todo => {
+        const todoItem = createTodoItem(todo.name, todo.description);
+        if (todo.completed) {
+            todoItem.classList.add('completed');
+            doneList.appendChild(todoItem);
+        } else {
+            remainingList.appendChild(todoItem);
+        }
+        });
+    })
+    .catch(error => console.error('Error loading to-dos:', error));
+}
+
+// Load existing todos from the API when the page loads
+loadTodosFromAPI();
